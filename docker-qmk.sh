b6a76470f340950f641a97d20237ea9d2790b531
greyhatmiddleman <whitehatmiddleman@gmail.com>
2021-11-12 05:07:50 +0000

    added bash to run docker qmk

---
+++ docker-qmk.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# to run this script just add the arguments after the script name
+# example: docker-qmk.sh compile -kb keyboard -km keymap
+
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+QMK_ROOT_FOLDER=$SCRIPT_DIR/qmk-firmware
+#QMK_ROOT_FOLDER=$SCRIPT_DIR/zsa-qmk
+#QMK_ROOT_FOLDER=$SCRIPT_DIR/vial-qmk
+
+docker run --rm -it -v $QMK_ROOT_FOLDER:/home/qmk/qmk_firmware user/qmk $@
