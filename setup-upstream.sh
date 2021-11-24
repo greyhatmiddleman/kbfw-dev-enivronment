b106f7cbc0c6a996bd33bb9add5771f4e020c741
greyhatmiddleman <whitehatmiddleman@gmail.com>
2021-11-24 09:21:36 +0000

    update scripts

--- setup-upstream.sh
+++ setup-upstream.sh
@@ -14,3 +14,9 @@ cd ..
 cd ./zsa-qmk
 git remote add upstream https://github.com/zsa/qmk_firmware.git
 cd ..
+
+echo "
+ qmk_firmware's default branch is master:    git merge upstream/master
+ vial-qmk default branch is vial:    git merge upstream/vial
+ zsa-qmk default branch is firmware20:    git merge upstream/firmware20
+ "
